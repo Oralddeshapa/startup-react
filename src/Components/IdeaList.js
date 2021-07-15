475a19f205166a43a99b76893a901f1b8c72cc64
Oralddeshapa <max_mus_2010@mail.ru>
2021-07-16 02:59:58 +0300

    init commit & connection to rails

---
+++ src/Components/IdeaList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class IdeaList extends React.Component {
+  state = {
+    ideas: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/api/v1/ideas`)
+      .then(res => {
+        this.setState({ ideas: res.data })
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+      { this.state.ideas.map( idea => <li>{ idea.title }</li> ) }
+      </ul>
+    )
+  }
+}
