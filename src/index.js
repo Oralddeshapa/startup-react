741dff2c697115967197a1afffd9753cd9b1da2d
Oralddeshapa <max_mus_2010@mail.ru>
2021-09-06 22:00:29 +0300

    small fixes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+console.log('1232131231')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
